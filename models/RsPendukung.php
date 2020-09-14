c1ccfc9d5a78005ed8f088088680cce3f8d0c54f
afdhal2705 <adfhal2705@gmail.com>
2020-09-14 10:10:44 +0700

    model kejiwaan

--- models/RsPendukung.php
+++ models/RsPendukung.php
@@ -8,9 +8,12 @@ use Yii;
  * This is the model class for table "mcu.rs_pendukung".
  *
  * @property int $id_rs
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
  * @property string $nama_rs
  * @property string $status
- * @property string $tanggal_created
  */
 class RsPendukung extends \yii\db\ActiveRecord
 {
@@ -28,8 +31,10 @@ class RsPendukung extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['nama_rs', 'status', 'tanggal_created'], 'required'],
-            [['tanggal_created'], 'safe'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['created_by', 'updated_by'], 'default', 'value' => null],
+            [['created_by', 'updated_by'], 'integer'],
+            [['nama_rs', 'status'], 'required'],
             [['nama_rs', 'status'], 'string', 'max' => 12],
         ];
     }
@@ -41,9 +46,12 @@ class RsPendukung extends \yii\db\ActiveRecord
     {
         return [
             'id_rs' => 'Id Rs',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
             'nama_rs' => 'Nama Rs',
             'status' => 'Status',
-            'tanggal_created' => 'Tanggal Created',
         ];
     }
 }
--- models/SpesialisKejiwaan.php
+++ models/SpesialisKejiwaan.php
@@ -5,22 +5,26 @@ namespace app\models;
 use Yii;
 
 /**
- * This is the model class for table "mcu.pemeriksaan_kejiwaan".
+ * This is the model class for table "mcu.spesialis_kejiwaan".
  *
- * @property int $id_pemeriksaan_kejiwaan
+ * @property int $id_spesialis_kejiwaan
  * @property string $no_rekam_medik
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
  * @property string $rs_pendukung
  * @property string $status
  * @property string $tanggal_created
  */
-class PemeriksaanKejiwaan extends \yii\db\ActiveRecord
+class SpesialisKejiwaan extends \yii\db\ActiveRecord
 {
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'mcu.pemeriksaan_kejiwaan';
+        return 'mcu.spesialis_kejiwaan';
     }
 
     /**
@@ -30,7 +34,9 @@ class PemeriksaanKejiwaan extends \yii\db\ActiveRecord
     {
         return [
             [['no_rekam_medik', 'rs_pendukung', 'status', 'tanggal_created'], 'required'],
-            [['tanggal_created'], 'safe'],
+            [['created_at', 'updated_at', 'tanggal_created'], 'safe'],
+            [['created_by', 'updated_by'], 'default', 'value' => null],
+            [['created_by', 'updated_by'], 'integer'],
             [['no_rekam_medik'], 'string', 'max' => 120],
             [['rs_pendukung', 'status'], 'string', 'max' => 12],
         ];
@@ -42,8 +48,12 @@ class PemeriksaanKejiwaan extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id_pemeriksaan_kejiwaan' => 'Id Pemeriksaan Kejiwaan',
+            'id_spesialis_kejiwaan' => 'Id Spesialis Kejiwaan',
             'no_rekam_medik' => 'No Rekam Medik',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
             'rs_pendukung' => 'Rs Pendukung',
             'status' => 'Status',
             'tanggal_created' => 'Tanggal Created',
