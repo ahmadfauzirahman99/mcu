f9049cdd5c32292a3c254b485c4d0077ebca98b4
Dicky Ermawan S <wanasaja@gmail.com>
2020-09-14 11:14:53 +0700

    push helper

--- components/Helper.php
+++ components/Helper.php
@@ -6,7 +6,7 @@
  * @Linkedin: linkedin.com/in/dickyermawan 
  * @Date: 2020-09-13 20:08:44 
  * @Last Modified by: Dicky Ermawan S., S.T., MTA
- * @Last Modified time: 2020-09-13 20:17:25
+ * @Last Modified time: 2020-09-14 09:51:12
  */
 
 namespace app\components;
@@ -15,7 +15,7 @@ class Helper
 {
     public static function radioList($index, $label, $name, $checked, $value, $model)
     {
-        $id = str_replace(['[', ']'], '-', $name) . $index;
+        $id = str_replace(['[', ']'], '_', $name) . $index;
         return \yii\helpers\Html::radio(
             $name,
             $checked,
