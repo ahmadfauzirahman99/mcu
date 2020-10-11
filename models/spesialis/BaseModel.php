2e21a1507fd3018af850f3f1917f0b221704e057
Dicky Ermawan S <wanasaja@gmail.com>
2020-10-11 16:00:56 +0700

    perbaiki select pasien (tambah no regis dan no daftar tidak null)

--- models/spesialis/BaseModel.php
+++ models/spesialis/BaseModel.php
@@ -6,7 +6,7 @@
  * @Linkedin: linkedin.com/in/dickyermawan 
  * @Date: 2020-09-13 12:12:24 
  * @Last Modified by: Dicky Ermawan S., S.T., MTA
- * @Last Modified time: 2020-10-07 13:14:29
+ * @Last Modified time: 2020-10-11 16:00:32
  */
 
 namespace app\models\spesialis;
@@ -20,12 +20,16 @@ class BaseModel extends Model
 
    public static function getListPasien()
    {
-       return ArrayHelper::map(DataLayanan::find()
-    //    ->select(['no_rekam_medik', 'concat("no_rekam_medik",\' / \',"nama",\' / No. Daftar: \',"no_registrasi") as nama'])
-       ->select(['no_rekam_medik', 'concat("no_rekam_medik",\' / \',"nama") as nama'])
-       ->select(['id_data_pelayanan', 'concat("no_rekam_medik",\' / \',"nama") as nama'])
-       ->orderBy(['no_registrasi' => SORT_DESC])
-       ->all(), 'id_data_pelayanan', 'nama');
-    //    ->all(), 'no_rekam_medik', 'nama');
+      return ArrayHelper::map(DataLayanan::find()
+         //  ->select(['no_rekam_medik', 'concat("no_rekam_medik",\' / \',"nama",\' / No. Daftar: \',"no_registrasi") as nama'])
+         ->select(['id_data_pelayanan', 'concat("no_rekam_medik",\' / \',"nama",\' / No. Daftar: \',"no_registrasi") as nama'])
+         //  ->select(['no_rekam_medik', 'concat("no_rekam_medik",\' / \',"nama") as nama'])
+         //  ->select(['id_data_pelayanan', 'concat("no_rekam_medik",\' / \',"nama") as nama'])
+         ->where([
+            'not', ['no_registrasi' => null]
+         ])
+         ->orderBy(['no_registrasi' => SORT_DESC])
+         ->all(), 'id_data_pelayanan', 'nama');
+      //    ->all(), 'no_rekam_medik', 'nama');
    }
 }
